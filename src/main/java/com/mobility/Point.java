441d422081a041e9ef2cd0f13489d471c71af7d0
Sage Baram <sagebaram@gamil.com>
2022-03-26 02:41:03 +0300

    (ref) modify package name, add checkBoundaries, ctor is now throwable if out of bounds

--- src/main/java/com/mobility/Point.java
+++ src/main/java/com/mobility/Point.java
@@ -1,4 +1,6 @@
-package mobility;
+package main.java.com.mobility;
+
+import main.java.com.privateutil.PointOutOfBoundException;
 
 /**
  * Point class
@@ -14,8 +16,25 @@ public class Point {
     private int y;
 
     /**
-     * Copy constructor
+     * Parameterized constructor
      * 
+     * @param x - int value of x coordinate.
+     * @param y - int value of y coordinate.
+     *          if not viable values, the constructor will initialize them with 0.
+     */
+    public Point(int x, int y) throws PointOutOfBoundException {
+        // see usage of point and figure out the default values.
+        if ((MIN_XY <=  x && x <= MAX_X) && (MIN_XY <= y && y <= MAX_Y)) {
+            this.x = x;
+            this.y = y;
+        } else {
+            throw new PointOutOfBoundException("Values are out of valid boundaries.");
+        }
+    }
+
+    /**
+     * Copy constructor
+     *
      * @param point - Point object to copy.
      */
     public Point(Point point) {
@@ -24,21 +43,14 @@ public class Point {
     }
 
     /**
-     * Parameterized constructor
-     * 
-     * @param x - int value of x coordinate.
-     * @param y - int value of y coordinate.
-     *          if not viable values, the constructor will initialize them with 0.
+     * isViable checks if a point is within valid boundaries.
+     *
+     * @param newLocation Point reference to examine.
+     * @return boolean value if the values are valid.
      */
-    public Point(int x, int y) {
-        // see usage of point and figure out the default values.
-        if (isViable(x, y)) {
-            this.x = x;
-            this.y = y;
-        } else {
-            this.x = 0;
-            this.y = 0;
-        }
+    public static boolean checkBoundaries(Point newLocation) {
+        return ((MIN_XY <= newLocation.getX() && newLocation.getX() <= MAX_X) &&
+                (MIN_XY <= newLocation.getY() && newLocation.getY() <= MAX_Y));
     }
 
     /**
@@ -60,28 +72,18 @@ public class Point {
     }
 
     /**
-     * isViable checks if a point is within valid boundries.
-     * 
-     * @param x - int value of x coordinate.
-     * @param y - int value of y coordinate.
-     * @return boolean value if the values are valid.
-     */
-    public boolean isViable(int x, int y) {
-        return ((MIN_XY <= x && x <= MAX_X) && (MIN_XY <= y && y <= MAX_Y));
-    }
-
-    /**
      * x setter.
      * 
      * @param x - int value of x coordinate.
      * @return boolean if x value is valid.
      */
     public boolean setX(int x) {
+        boolean isSuccess = false;
         if (MIN_XY <= x && x <= MAX_X) {
             this.x = x;
-            return true;
+            isSuccess = true;
         }
-        return false;
+        return isSuccess;
 
     }
 
@@ -92,12 +94,12 @@ public class Point {
      * @return boolean if y value is valid.
      */
     public boolean setY(int y) {
+        boolean isSuccess = false;
         if (MIN_XY <= y && y <= MAX_Y) {
             this.y = y;
-            return true;
-        } else {
-            return false;
+            isSuccess = true;
         }
+        return isSuccess;
     }
 
     /**
@@ -114,10 +116,9 @@ public class Point {
     public boolean equals(Object obj) {
         if (obj == this)
             return true;
-        if (!(obj instanceof Point))
+        if (!(obj instanceof Point point))
             return false;
         else {
-            Point point = (Point) obj;
             return (this.getX() == point.getX() && this.getY() == point.getY());
         }
 
