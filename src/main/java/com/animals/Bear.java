d68721e18765a819004387773626a39c59634436
Sage Baram <sagebaram@gamil.com>
2022-03-26 01:53:52 +0300

    (feat) implementation of animal descendants

--- src/main/java/com/animals/Bear.java
+++ src/main/java/com/animals/Bear.java
@@ -1,1 +1,74 @@
-package animals;
+package main.java.com.animals;
+
+import main.java.com.diet.Omnivore;
+import main.java.com.food.EFoodType;
+import main.java.com.mobility.Point;
+import main.java.com.utilities.MessageUtility;
+
+/**
+ * Bear
+ */
+public class Bear extends AnimalRoar {
+    private static final double DEFAULT_WEIGHT = 308.2;
+    private static final Point DEFAULT_STARTING_LOCATION = new Point(100, 5);
+    private FurColors furColor;
+
+    private enum FurColors {
+        WHITE("WHITE"),
+        GRAY("GRAY"),
+        BLACK("BLACK");
+
+        private final String color;
+
+        private FurColors(String color) {
+            this.color = color;
+        }
+
+        public String getColor() {
+            return color;
+        }
+    }
+
+    public Bear(String name, Point location) {
+        super(name, location);
+        setWeight(DEFAULT_WEIGHT);
+        setFurColor(FurColors.GRAY.toString());
+        setDiet(new Omnivore());
+        MessageUtility.logConstractor(getClass().getSimpleName(), name);
+    }
+
+    public Bear(String name) {
+        super(name, DEFAULT_STARTING_LOCATION);
+        setWeight(DEFAULT_WEIGHT);
+        setFurColor(FurColors.GRAY.toString());
+        setDiet(new Omnivore());
+        MessageUtility.logConstractor(getClass().getSimpleName(), name);
+    }
+
+    public boolean setFurColor(String furColor) {
+        boolean isSuccess = false;
+        for (FurColors f : FurColors.values()) {
+            if (f.name().equals(furColor)) {
+                isSuccess = true;
+            }
+        }
+        MessageUtility.logSetter(getName(), "setFurColor", getFurColor(), isSuccess);
+        return isSuccess;
+    }
+
+    public FurColors getFurColor() {
+        MessageUtility.logGetter(getName(), "getFurColor", furColor.getColor());
+        return furColor;
+    }
+
+    @Override
+    public void roar() {
+        MessageUtility.logSound(getName(), "Stands on its hind legs, roars and scratches its belly");
+    }
+
+    @Override
+    public EFoodType getFoodtype() {
+        MessageUtility.logGetter(getName(), "getFoodtype", EFoodType.MEAT);
+        return EFoodType.MEAT;
+    }
+}
--- src/main/java/com/animals/Lion.java
+++ src/main/java/com/animals/Lion.java
@@ -1,33 +1,59 @@
-package animals;
+package main.java.com.animals;
 
-public class Lion extends Animal {
+import main.java.com.food.EFoodType;
+import main.java.com.food.IEdible;
+import main.java.com.utilities.MessageUtility;
+import main.java.com.mobility.Point;
+
+import java.util.Random;
+
+public class Lion extends AnimalRoar {
     private static final double DEFAULT_WEIGHT = 408.2;
     private static final Point DEFAULT_STARTING_LOCATION = new Point(20, 0);
+    private static final int DEFAULT_SCAR_COUNT = 0;
 
     private int scarCount;
 
+    public Lion(String name, Point location) {
+        super(name, location);
+        setWeight(DEFAULT_WEIGHT);
+        scarCount = DEFAULT_SCAR_COUNT;
+        MessageUtility.logConstractor(getClass().getSimpleName(), name);
+    }
+
     public Lion(String name) {
-        super(name, DEFAULT_WEIGHT, DEFAULT_STARTING_LOCATION);
-        scarCount = 0;
-    }
-
-    private void roar() {
-        MessageUtility.logSound(this.getName(), "Roars, then stretches and shakes its mane");
-    }
-
-    @Override
-    public void makeSound() {
-        roar();
+        super(name, DEFAULT_STARTING_LOCATION);
+        setWeight(DEFAULT_WEIGHT);
+        scarCount = DEFAULT_SCAR_COUNT;
+        MessageUtility.logConstractor(getClass().getSimpleName(), name);
     }
 
     @Override
     public boolean eat(IEdible food) {
-        // TODO Auto-generated method stub
-        return false;
+        boolean isSuccess = false;
+        if (super.eat(food)) {
+            Random random = new Random();
+            if (random.nextInt(2) == 0) {
+                this.scarCount++;
+                isSuccess = true;
+            }
+        }
+        MessageUtility.logBooleanFunction(getName(), "eat", food, isSuccess);
+        return isSuccess;
     }
 
     @Override
-    public EFoodType getFoodType() {
+    public void roar() {
+        MessageUtility.logSound(getName(), "Roars, then stretches and shakes its mane");
+    }
 
+    @Override
+    public EFoodType getFoodtype() {
+        return EFoodType.NOTFOOD;
+    }
+
+    public int getScarCount() {
+        MessageUtility.logGetter(getName(), "getScarCount", scarCount);
+        return scarCount;
     }
 }
