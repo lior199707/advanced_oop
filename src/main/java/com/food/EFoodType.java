3fb20439caa7bcca452327d336b69a750c160936
Sage Baram <sagebaram@gamil.com>
2022-03-26 02:00:12 +0300

    (ref) modify package name, enum made public for external usage.

--- src/main/java/com/food/EFoodType.java
+++ src/main/java/com/food/EFoodType.java
@@ -1,11 +1,11 @@
-package food;
+package main.java.com.food;
 
 /**
  * MEAT - all animals excluding lions.
  * NOTFOOD - lions.
  * VEGETABLE - all plants.
  */
-enum EFoodType {
+public enum EFoodType {
     MEAT,
     NOTFOOD,
     VEGETABLE
