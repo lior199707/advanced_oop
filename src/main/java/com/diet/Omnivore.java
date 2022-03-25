7d8d949b0fc38d93693a75bb9507995bcf016b21
Sage Baram <sagebaram@gamil.com>
2022-03-26 01:59:05 +0300

    (feat) implement of IDiet methods, add imports. avoiding code dupe.

--- src/main/java/com/diet/Omnivore.java
+++ src/main/java/com/diet/Omnivore.java
@@ -1,18 +1,29 @@
+package main.java.com.diet;
+
+import main.java.com.animals.Animal;
+import main.java.com.food.IEdible;
+import main.java.com.food.EFoodType;
+
 /**
  * All-type eating animals.
  */
 public class Omnivore implements IDiet {
     @Override
     public double eat(Animal animal, IEdible food) {
-        // eatting meat type increase animal weight by 10%
-        // eatting veggies increase animal weight by 7%
-
+        if (canEat(food.getFoodtype())) {
+            if (food.getFoodtype() == EFoodType.MEAT) {
+                Carnivore carnivore = new Carnivore();
+                return carnivore.eat(animal, food);
+            } else {
+                Herbivore herbivore = new Herbivore();
+                return herbivore.eat(animal, food);
+            }
+        }
         return 0;
     }
 
     @Override
     public boolean canEat(EFoodType food) {
-        // can eat all except NOTFOOD.
-        return false;
+        return food == EFoodType.MEAT || food == EFoodType.VEGETABLE;
     }
 }
