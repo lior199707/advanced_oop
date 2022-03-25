07fbc95363ab8b9ddbd48ab7342c3734dc1db323
Sage Baram <sagebaram@gamil.com>
2022-03-26 02:42:52 +0300

    (feat) add user-made exception class for point values out of bound.

---
+++ src/main/java/com/privateutil/PointOutOfBoundException.java
@@ -0,0 +1,7 @@
+package main.java.com.privateutil;
+
+public class PointOutOfBoundException extends Exception {
+    public PointOutOfBoundException(String message){
+        super(message);
+    }
+}
