8c99bb73b51656f052e9ebacd6a8614cb9881473
Sage Baram <sagebaram@gamil.com>
2022-03-27 00:56:47 +0300

    (feat) test static move method of ZooActions

--- src/test/java/ZooActionsTesting.java
+++ src/test/java/ZooActionsTesting.java
@@ -1,6 +1,7 @@
 import com.animals.Giraffe;
 import com.animals.Lion;
 import com.food.IEdible;
+import com.mobility.Point;
 import com.zoo.ZooActions;
 import org.junit.jupiter.api.Test;
 
@@ -8,10 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ZooActionsTesting {
     @Test
-    public void testFindAnimalType(){
+    public void testEatAnimal(){
         Object animal = new Lion("Simba");
         IEdible animal2 = new Giraffe("Sendi");
         assertTrue(ZooActions.eat(animal, animal2));
     }
 
+    @Test
+    public void testMoveAnimal(){
+        Object animal = new Lion("Simba");
+        Point location = new Point(30,0);
+        assertTrue(ZooActions.move(animal, location));
+    }
 }
