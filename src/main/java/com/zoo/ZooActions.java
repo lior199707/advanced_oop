bfe9d424b2716b7ec24b33c49a02996cb5df0e5a
Lior <Shilon199707@gmail.com>
2022-03-26 23:41:01 +0300

    (feat) add helper method to find animal instance type

--- src/main/java/com/zoo/ZooActions.java
+++ src/main/java/com/zoo/ZooActions.java
@@ -1,8 +1,35 @@
 package com.zoo;
 
+import com.animals.*;
+import com.food.IEdible;
+
 public class ZooActions
 {
+
+    private static Animal findAnimalInstance(Object animal) {
+        if (animal instanceof Lion)
+            return (Lion) animal;
+        if (animal instanceof Bear)
+            return (Bear) animal;
+        if (animal instanceof Giraffe)
+            return (Giraffe) animal;
+        if (animal instanceof Elephant)
+            return (Elephant) animal;
+        if (animal instanceof Turtle)
+            return (Turtle) animal;
+        return null;
+    }
+
+
+
+    public static boolean eat(Object animal, IEdible food){
+        Animal creature = findAnimalInstance(animal);
+        return creature.eat(food);
+    }
+
+
     public static void main(String[] args) {
+
         
     }
 
