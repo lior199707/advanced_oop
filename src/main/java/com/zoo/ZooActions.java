64ee17dd6d0386436924ebbeb13765f2336b2805
Sage Baram <sagebaram@gamil.com>
2022-03-29 22:48:08 +0300

    (feat) implemented reflection API to instantiate animals, using static move & eat methods in main.

--- src/main/java/com/zoo/ZooActions.java
+++ src/main/java/com/zoo/ZooActions.java
@@ -1,131 +1,148 @@
 package com.zoo;
 
-import com.animals.*;
+import com.animals.Animal;
 import com.food.IEdible;
 import com.mobility.Point;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Random;
 import java.util.Scanner;
 
-public class ZooActions
-{
+public class ZooActions {
+    private static final Scanner sc = new Scanner(System.in);
 
-    private static Animal findAnimalInstance(Object animal) {
-        if (animal instanceof Lion)
-            return (Lion) animal;
-        if (animal instanceof Bear)
-            return (Bear) animal;
-        if (animal instanceof Giraffe)
-            return (Giraffe) animal;
-        if (animal instanceof Elephant)
-            return (Elephant) animal;
-        if (animal instanceof Turtle)
-            return (Turtle) animal;
-        return null;
-    }
-
-
-
-    public static boolean eat(Object animal, IEdible food){
-        Animal creature = findAnimalInstance(animal);
+    public static boolean eat(Object animal, IEdible food) {
+        Animal creature = (Animal) animal;
         return creature.eat(food);
     }
 
-    public static boolean move(Object animal, Point point){
-        Animal creature = findAnimalInstance(animal);
+    public static boolean move(Object animal, Point point) {
+        Animal creature = (Animal) animal;
         double distanceTraveled = creature.move(point);
         return distanceTraveled != 0;
     }
+//
+    
+    public static Animal loadAnimalWithName(String type, String name){
+        Animal animal = null;
+        try {
+            ClassLoader clsLoader = ClassLoader.getSystemClassLoader();
+            Class c = clsLoader.loadClass("com.animals." + type);
+            Constructor<Animal> con = c.getConstructor(String.class);
+            animal = con.newInstance(name);
+        } catch (NoSuchMethodException | ClassNotFoundException e) {
+            System.out.println(e + " unable to load animal");
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return animal;
+    }
 
-    private void createAnimalArray(Animal[] animals, int size){
+    public static Animal loadAnimalWithNameAndPoint(String type, String name, Point location){
+        Animal animal = null;
+        try {
+            ClassLoader clsLoader = ClassLoader.getSystemClassLoader();
+            Class c = clsLoader.loadClass("com.animals." + type);
+            Constructor<Animal> con = c.getConstructor(String.class, Point.class);
+            animal = con.newInstance(name, location);
+        } catch (NoSuchMethodException | ClassNotFoundException e) {
+            System.out.println(e + " unable to load animal");
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return animal;
+    }
+    private static Animal[] createAnimalArray(int size) {
+        Animal[] animals = new Animal[size];
         int userChoice;
-        int pointChoise;
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < size; i++) {
-            System.out.println("Please choose the " + (i+1) + "animal");
+        int pointChoice;
+        for (int i = 0; i < size; i++) {
+            System.out.println("Selecting animal number " + (i + 1) + " or exit program\n");
             System.out.println("1. Lion");
             System.out.println("2. Bear");
             System.out.println("3. Giraffe");
             System.out.println("4. Turtle");
             System.out.println("5. Elephant");
             System.out.println("6. Exit Program");
-            userChoice = scanner.nextInt();
-            while (userChoice < 0 || userChoice > 6){
-                System.out.println("The number you chose isn't valid, please try again");
-                userChoice = scanner.nextInt();
+            System.out.print("Choose an option: ");
+            userChoice = sc.nextInt();
+
+            // checking if userChoice is in boundaries.
+            while (userChoice < 0 || userChoice > 6) {
+                System.out.println("Invalid option, please try again");
+                userChoice = sc.nextInt();
             }
-            if(userChoice == 6){
+
+            // exiting before initiation of array.
+            if (userChoice == 6) {
                 System.out.println("Goodbye");
                 System.exit(1);
             }
-            System.out.println("Please enter the animal's name:");
-            String name = scanner.nextLine();
-            System.out.println("Do you wish to enter the animal's spawn point?");
-            System.out.println("1. Yes");
-            System.out.println("2. No");
-            pointChoise = scanner.nextInt();
-            while (pointChoise < 0 || pointChoise > 2) {
-                System.out.println("The number you chose isn't valid, please try again");
-                pointChoise = scanner.nextInt();
-            }
-            switch (pointChoise){
-                case 1:
-                    System.out.println("Please enter the x value:");
-                    int x = scanner.nextInt();
-                    System.out.println("Please enter the y value:");
-                    int y = scanner.nextInt();
-                    Point location = new Point(x,y);
-                    switch (userChoice){
-                        case 1 ->
-                                // create Lion
-                                animals[i] = new Lion(name,location);
-                        case 2 ->
-                                // create Bear
-                                animals[i] = new Bear(name,location);
-                        case 3 ->
-                                // create Giraffe
-                                animals[i] = new Giraffe(name,location);
-                        case 4 ->
-                                // create Turtle
-                                animals[i] = new Turtle(name,location);
-                        case 5 ->
-                                // create Elephant
-                                animals[i] = new Elephant(name,location);
-                    }
-                    break;
-                case 2:
-                    switch (userChoice) {
-                        case 1 ->
-                                // create Lion
-                                animals[i] = new Lion(name);
-                        case 2 ->
-                                // create Bear
-                                animals[i] = new Bear(name);
-                        case 3 ->
-                                // create Giraffe
-                                animals[i] = new Giraffe(name);
-                        case 4 ->
-                                // create Turtle
-                                animals[i] = new Turtle(name);
-                        case 5 ->
-                                // create Elephant
-                                animals[i] = new Elephant(name);
-                    }
-            }
-        }
-    }
 
+            String type = null;
+            switch (userChoice) {
+                case 1 -> type = "Lion";
+                case 2 -> type = "Bear";
+                case 3 -> type = "Giraffe";
+                case 4 -> type = "Turtle";
+                case 5 -> type = "Elephant";
+            }
+
+
+            // input animal name.
+            System.out.print("Please enter the animal's name: ");
+            String name = sc.next();
+
+            System.out.println("Do you wish to enter the animal's spawn point?");
+            System.out.println("1. No");
+            System.out.println("2. Yes");
+            pointChoice = sc.nextInt();
+
+            while (pointChoice < 0 || pointChoice > 2) {
+                System.out.println("The number you chose isn't valid, please try again");
+                pointChoice = sc.nextInt();
+            }
+
+            if (pointChoice == 1) {
+                animals[i] = loadAnimalWithName(type,name);
+            } else {
+                Point location = Point.pointInput();
+                animals[i] = loadAnimalWithNameAndPoint(type,name,location);
+            }
+
+        }
+        return animals;
+    }
 
     public static void main(String[] args) {
-        final int MIN_NUM_OF_ANIMALS = 3;
-        Scanner scanner = new Scanner(System.in);
+        final int MIN_NUM_OF_ANIMALS = 1;
+        Random rand = new Random();
         int size = 0;
         while(size < MIN_NUM_OF_ANIMALS){
-            System.out.println("please enter the the number of animals, the minimum number of animals is 3");
-            size = scanner.nextInt();
+            System.out.println("Please enter the number of animals (MINIMUM OF 3): ");
+            size = sc.nextInt();
         }
-        Animal[] animalArr = new Animal[size];
 
+        Animal[] animals = createAnimalArray(size);
 
+        for (Animal animal : animals){
+            System.out.println(animal.getLocation());
+        }
+
+        for (Animal animal : animals){
+            Point nextLocation = Point.pointInput();
+            if (ZooActions.move(animal,nextLocation)){
+                animal.getWeight();
+            }
+        }
+
+        int raffle = size/2;
+        for (int i = 0; i < raffle; i++){
+            int one = rand.nextInt(size);
+            int two = rand.nextInt(size);
+            ZooActions.eat(animals[one], animals[two]);
+        }
+        sc.close();
     }
-
 }
