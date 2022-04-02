a12523110c39531b849f2d221744eb09e12f2d22
Lior <Shilon199707@gmail.com>
2022-04-03 02:09:06 +0300

    (test) add test to set location

--- src/test/java/SettersTesting.java
+++ src/test/java/SettersTesting.java
@@ -1,3 +1,4 @@
+import com.mobility.Point;
 import com.privateutil.PrivateUtils;
 import org.junit.Test;
 import com.animals.*;
@@ -27,7 +28,7 @@ public class SettersTesting {
         assertFalse(elephant.setName("1234"));
     }
     @Test
-    public void setWeight(){
+    public void setWeightTesting(){
         assertTrue(lion.setWeight(1));
         assertTrue(turtle.setWeight(2));
         assertTrue(bear.setWeight(100));
@@ -40,7 +41,7 @@ public class SettersTesting {
         assertFalse(elephant.setWeight(-100));
     }
     @Test
-    public void setFurForBear(){
+    public void setFurForBearTesting(){
         Bear bear1 = (Bear) bear;
         assertTrue(bear1.setFurColor("WHITE"));
         assertTrue(bear1.setFurColor("GRAY"));
@@ -49,7 +50,7 @@ public class SettersTesting {
     }
 
     @Test
-    public void setTrunkLengthForElephant(){
+    public void setTrunkLengthForElephantTesting(){
         Elephant elph = (Elephant) elephant;
         assertTrue(elph.setTrunkLength(0.5));
         assertFalse(elph.setTrunkLength(0.49));
@@ -59,7 +60,7 @@ public class SettersTesting {
     }
 
     @Test
-    public void setNeckLengthForGiraffe(){
+    public void setNeckLengthForGiraffeTesting(){
         Giraffe giraffe1 = (Giraffe) giraffe;
         assertTrue(giraffe1.setNeckLength(1));
         assertFalse(giraffe1.setNeckLength(0.99));
@@ -68,12 +69,31 @@ public class SettersTesting {
         assertFalse(giraffe1.setNeckLength(-1));
     }
 
-    @Test public void setAgeForTurtle(){
+    @Test public void setAgeForTurtleTesting(){
         Turtle torty = (Turtle) turtle;
         assertTrue(torty.setAge(0));
         assertFalse(torty.setAge(-1));
         assertTrue(torty.setAge(500));
         assertFalse(torty.setAge(501));
         assertFalse(torty.setAge(-1));
+    }
+
+    @Test public void setLocationTesting(){  //x: 800 , y:600
+        assertTrue(lion.setLocation(new Point(800, 600)));
+        assertTrue(turtle.setLocation(new Point(800, 600)));
+        assertTrue(elephant.setLocation(new Point(800, 600)));
+        assertTrue(bear.setLocation(new Point(800, 600)));
+        assertTrue(giraffe.setLocation(new Point(800, 600)));
+        assertTrue(lion.setLocation(new Point(0, 0)));
+        assertTrue(turtle.setLocation(new Point(0, 0)));
+        assertTrue(elephant.setLocation(new Point(0, 0)));
+        assertTrue(bear.setLocation(new Point(0, 0)));
+        assertTrue(giraffe.setLocation(new Point(0, 0)));
+        assertFalse(lion.setLocation(new Point(801, 10)));
+        assertFalse(turtle.setLocation(new Point(10, 601)));
+        assertFalse(elephant.setLocation(new Point(-1, -1)));
+        assertFalse(bear.setLocation(new Point(801, 601)));
+        assertFalse(giraffe.setLocation(new Point(1000, 900)));
+
     }
 }
