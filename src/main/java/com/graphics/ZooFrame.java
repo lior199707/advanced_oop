026d0e3509e5b77245688dd9d97d255c32fc73d0
Sage Baram <sagebaram@gamil.com>
2022-04-05 20:35:11 +0300

    (feat) add ZooMenuBar and ZooFrame

--- src/main/java/com/graphics/ZooFrame.java
+++ src/main/java/com/graphics/ZooFrame.java
@@ -1,10 +1,22 @@
 package com.graphics;
 
+import com.graphics.menu.ZooMenuBar;
+
 import javax.swing.*;
 
 public class ZooFrame extends JFrame {
+    private JMenuBar menuBar;
+    private ZooPanel zooPanel;
+
 
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame();
+        JMenuBar menuBar = new ZooMenuBar();
+        frame.setJMenuBar(menuBar);
+        frame.setTitle("Zooshit");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
