8f5a394e4d610c37416af9a97ba97a29f99fcfad
Sage Baram <sagebaram@gamil.com>
2022-04-07 23:51:09 +0300

    (feat) set up zoo frame and zoo panel

--- src/main/java/com/graphics/ZooFrame.java
+++ src/main/java/com/graphics/ZooFrame.java
@@ -1,22 +1,98 @@
 package com.graphics;
 
-import com.graphics.menu.ZooMenuBar;
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ZooFrame extends JFrame {
-    private JMenuBar menuBar;
-    private ZooPanel zooPanel;
+public class ZooFrame extends JFrame{
+    private final JMenuBar menuBar;
+    private final JPanel zooPanel;
+
+    ZooFrame(){
+        menuBar = constructJMenuBar();
+        zooPanel = new ZooPanel();
+
+        this.add(zooPanel, BorderLayout.SOUTH);
+        this.setJMenuBar(menuBar);
+
+        this.setTitle("Zooshit");
+        this.setSize(1000,1000);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private JMenuBar constructJMenuBar( ){
+        JMenuBar menuBar = new JMenuBar();
+        // initiating JMenu fields.
+
+        JMenu file = new JMenu("File");
+        JMenu background = new JMenu("Background");
+        JMenu help = new JMenu("Help");
+
+        // initiating items for file menu.
+        JMenuItem exit = new JMenuItem("Exit");
+
+        // initiating items for background menu.
+        JMenuItem image = new JMenuItem("Image");
+        JMenuItem green = new JMenuItem("Green");
+        JMenuItem none = new JMenuItem("None");
+
+        // initiating items for help menu.
+        JMenuItem helpMenuItem = new JMenuItem("Help");
 
 
+        file.add(exit);
+
+        background.add(image);
+        background.add(green);
+        background.add(none);
+
+        help.add(helpMenuItem);
+
+        // adding menus to the menu bar.
+        menuBar.add(file);
+        menuBar.add(background);
+        menuBar.add(help);
+
+        ZooMenuBarListener listener = new ZooMenuBarListener();
+        exit.addActionListener(listener);
+        image.addActionListener(listener);
+        green.addActionListener(listener);
+        none.addActionListener(listener);
+        helpMenuItem.addActionListener(listener);
+
+        return menuBar;
+    }
+
+    private class ZooMenuBarListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String actionCommand = e.getActionCommand();
+            switch (actionCommand) {
+                case  "Exit" -> {
+                    System.exit(1);
+                }
+                case "Image" -> {
+                    System.out.println("Background -> Image pressed.");
+                }
+                case "Green" -> {
+                    System.out.println("Background -> Green pressed.");
+                    zooPanel.setBackground(Color.green);
+                }
+                case "None" -> {
+                    System.out.println("Background -> None pressed!");
+                    zooPanel.setBackground(null);
+                }
+                case "Help" -> {
+                    JOptionPane.showMessageDialog(null,"Home Work 2\nGUI", "Message", JOptionPane.INFORMATION_MESSAGE);
+                    System.out.println("Help -> Help pressed.");
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JMenuBar menuBar = new ZooMenuBar();
-        frame.setJMenuBar(menuBar);
-        frame.setTitle("Zooshit");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,400);
-        frame.setLayout(null);
+        JFrame frame = new ZooFrame();
+//        frame.setLayout(new BorderLayout(100,100));
         frame.setVisible(true);
     }
 }
--- src/main/java/com/graphics/ZooPanel.java
+++ src/main/java/com/graphics/ZooPanel.java
@@ -1,11 +1,82 @@
 package com.graphics;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ZooPanel extends JPanel implements Runnable {
+public class ZooPanel extends JPanel implements Runnable, ActionListener {
+    private JPanel actionPanel;
+
+    public ZooPanel(){
+        actionPanel = new JPanel();
+//        animalPanel = new JPanel();
+        // instantiating buttons
+        JButton addAnimal = new JButton("Add Animal");
+        JButton moveAnimal = new JButton("Move Animal");
+        JButton clear = new JButton("Clear");
+        JButton food = new JButton("Food");
+        JButton info = new JButton("Info");
+        JButton exit = new JButton("Exit");
+
+        // adding to action listener
+        addAnimal.addActionListener(this);
+        moveAnimal.addActionListener(this);
+        clear.addActionListener(this);
+        food.addActionListener(this);
+        info.addActionListener(this);
+        exit.addActionListener(this);
+
+        // adding buttons to panel
+        actionPanel.add(addAnimal);
+        actionPanel.add(moveAnimal);
+        actionPanel.add(clear);
+        actionPanel.add(food);
+        actionPanel.add(info);
+        actionPanel.add(exit);
+
+        // setting properties
+        Font courier = new Font("Courier", Font.PLAIN, 14);
+        TitledBorder actionBorder = BorderFactory.createTitledBorder("Select Option:");
+        actionBorder.setTitleFont(courier);
+        actionBorder.setTitleColor(Color.blue);
+
+        actionPanel.setBorder(actionBorder);
+        this.add(actionPanel);
+    }
 
     @Override
     public void run() {
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String actionCommand = e.getActionCommand();
+        switch (actionCommand) {
+            case "Add Animal" -> {
+                JDialog dialog = AddAnimalDialog.getInstance();
+                dialog.getContentPane();
+                System.out.println("Add Animal pressed!");
+            }
+            case "Move Animal" -> {
+                JDialog dialog = MoveAnimalDialog.getInstance();
+                dialog.getContentPane();
+                System.out.println("Move Animal pressed!");
+            }
+            case "Clear" -> {
+                System.out.println("Clear pressed!");
+            }
+            case "Food" -> {
+                System.out.println("Food pressed!");
+            }
+            case "Info" -> {
+                System.out.println("Info pressed!");
+            }
+            case "Exit" -> {
+                System.exit(1);
+            }
+        }
     }
 }
