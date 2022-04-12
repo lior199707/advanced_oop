5b671b72d6611c44e77665773f5bc705dcc3bdfd
Lior <Shilon199707@gmail.com>
2022-04-12 17:12:49 +0300

    (feat) add test for move animal panel, test for creating a table of the zoo animals

--- src/main/java/com/graphics/ZooPanel.java
+++ src/main/java/com/graphics/ZooPanel.java
@@ -5,10 +5,12 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ZooPanel extends JPanel implements Runnable, ActionListener {
     private JPanel actionPanel;
     private JDialog addAnimalDialog;
+    private JFrame tableFrame;
 
     public ZooPanel(){
         actionPanel = new JPanel();
@@ -47,6 +49,30 @@ public class ZooPanel extends JPanel implements Runnable, ActionListener {
         this.add(actionPanel);
     }
 
+    private void createFoodDialog(){
+//        Object[] possibilities = {"Lettuce", "Cabbage", "Meat"};
+//        String s = (String)JOptionPane.showInputDialog(
+//                null,
+//                "What food would you like to add?",
+//                "Add Food Dialog",
+//                JOptionPane.QUESTION_MESSAGE,
+//                null,
+//                possibilities,
+//                "ham");
+        Object[] options = {"Lettuce", "Cabbage", "Meat"};
+        int result = JOptionPane.showOptionDialog(null,//parent container of JOptionPane
+                "What food would you like to add?",
+                "Add Food Dialog",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,//do not use a custom Icon
+                options,//the titles of buttons
+                null);
+        if (result == JOptionPane.YES_OPTION){System.out.println("Lettuce");}
+        else if (result == JOptionPane.NO_OPTION){System.out.println("Cabbage");}
+        else if (result == JOptionPane.CANCEL_OPTION){System.out.println("Meat");}
+    }
+
     @Override
     public void run() {
 
@@ -60,7 +86,18 @@ public class ZooPanel extends JPanel implements Runnable, ActionListener {
                 addAnimalDialog = new AddAnimalDialog();
             }
             case "Move Animal" -> {
-                JDialog dialog = MoveAnimalDialog.getInstance();
+                ArrayList<String> names = new ArrayList<>();
+                names.add("name1");
+                names.add("name2");
+                names.add("name3");
+                names.add("name4");
+                names.add("name5");
+                names.add("name6");
+                names.add("name7");
+                names.add("name8");
+                names.add("name9");
+                names.add("name10");
+                JDialog dialog = new MoveAnimalDialog(names);
                 dialog.getContentPane();
                 System.out.println("Move Animal pressed!");
             }
@@ -68,9 +105,33 @@ public class ZooPanel extends JPanel implements Runnable, ActionListener {
                 System.out.println("Clear pressed!");
             }
             case "Food" -> {
+                createFoodDialog();
                 System.out.println("Food pressed!");
             }
             case "Info" -> {
+                //creating animals details list
+                ArrayList<ArrayList<String>> animalsDetails = new ArrayList<>();
+                //creating first animal details
+                ArrayList<String> animalDetails1 = new ArrayList<>();
+                animalDetails1.add("Elephant");
+                animalDetails1.add("NATURAL");
+                animalDetails1.add("2000");
+                animalDetails1.add("5");
+                animalDetails1.add("5");
+                animalDetails1.add("4");
+                //creating second animal details
+                ArrayList<String> animalDetails2 = new ArrayList<>();
+                animalDetails2.add("Lion");
+                animalDetails2.add("NATURAL");
+                animalDetails2.add("380");
+                animalDetails2.add("8");
+                animalDetails2.add("8");
+                animalDetails2.add("5");
+                //adding the details to the main array
+                animalsDetails.add(animalDetails1);
+                animalsDetails.add(animalDetails2);
+                tableFrame = new ZooTable(animalsDetails, 9);
+
                 System.out.println("Info pressed!");
             }
             case "Exit" -> {
