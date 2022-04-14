c749ee34dd42677687b32f0e8b2e71ff19f9f9f4
Sage Baram <sagebaram@gamil.com>
2022-04-15 01:15:48 +0300

    (ref) add relative path to picture path, change loadImages param name

--- src/main/java/com/graphics/IDrawable.java
+++ src/main/java/com/graphics/IDrawable.java
@@ -3,8 +3,8 @@ package com.graphics;
 import java.awt.*;
 
 public interface IDrawable {
-    public final static String PICTURE_PATH = "...";
-    public void loadImages(String nm);
+    public final static String PICTURE_PATH = "src/main/resources/assignment2_pictures/";
+    public void loadImages(String shortPathName);
     public void drawObject (Graphics g);
     public String getColor();
 }
