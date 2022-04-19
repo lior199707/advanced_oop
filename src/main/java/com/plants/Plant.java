b0ba7524e9e3020c868d86bde97478cf6bdd3bde
Sage Baram <sagebaram@gamil.com>
2022-04-19 15:48:49 +0300

    (ref) center plant image, add panel setter instead of ctor param

--- src/main/java/com/plants/Plant.java
+++ src/main/java/com/plants/Plant.java
@@ -43,13 +43,15 @@ public abstract class Plant implements IEdible, Ilocatable, IDrawable {
 	 */
 	public Plant() {
 		Random rand = new Random();
-		int x = rand.nextInt(30);
-		int y = rand.nextInt(12);
-		this.location = new Point(x, y);
 		this.height = rand.nextInt(30);
 		this.weight = rand.nextInt(12);
+		this.location = new Point(Point.getMaxX()/2, Point.getMaxY()/2);
 		loadImages(foodShortPathName());
 		MessageUtility.logConstractor("Plant", "Plant");
+	}
+
+	public void setPan(ZooPanel pan) {
+		this.pan = pan;
 	}
 
 	public abstract String foodShortPathName();
@@ -124,7 +126,7 @@ public abstract class Plant implements IEdible, Ilocatable, IDrawable {
 
 	/**
 	 * @param weight double value representing plant weight.
-	 * @return boolean value indicating if the set was succcesful or not.
+	 * @return boolean value indicating if the set was successful or not.
 	 */
 	public boolean setWeight(double weight) {
 		boolean isSuccess = (weight >= 0);
@@ -141,9 +143,7 @@ public abstract class Plant implements IEdible, Ilocatable, IDrawable {
 	@Override
 	public void drawObject(Graphics g) {
 		// need to draw the object at the center of the screen.
-		int centerX = pan.getWidth() / 2;
-		int centerY = pan.getHeight() / 2;
-		g.drawImage(img, centerX, centerY, null);
+		g.drawImage(img, location.getX() + 80, location.getY() + 60, 50,50,pan);
 	}
 
 	@Override
@@ -156,10 +156,6 @@ public abstract class Plant implements IEdible, Ilocatable, IDrawable {
 					JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE,
 					null, null, null);
 		}
-	}
-
-	public BufferedImage getImg() {
-		return img;
 	}
 
 	@Override
