13ce9b209e327cc893a96243654b43f98172a4d1
Sage Baram <sagebaram@gamil.com>
2022-04-19 20:18:13 +0300

    (feat) add Food abstract class, Plant extends from Food, clean Plant

---
+++ src/main/java/com/food/plants/Plant.java
@@ -0,0 +1,30 @@
+package com.food.plants;
+
+import com.food.EFoodType;
+import com.food.Food;
+import com.utilities.MessageUtility;
+
+/**
+ * @author baroh
+ *
+ */
+public abstract class Plant extends Food {
+	/**
+	 * 
+	 */
+	public Plant() {
+		MessageUtility.logConstractor("Plant", "Plant");
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see food.IFood#getFoodtype()
+	 */
+	@Override
+	public EFoodType getFoodtype() {
+		MessageUtility.logGetter(this.getClass().getSimpleName(), "getFoodType", EFoodType.VEGETABLE);
+		return EFoodType.VEGETABLE;
+	}
+}
