2593d299556efdf9e7a72c76e1a904a15209f0c0
Sage Baram <sagebaram@gamil.com>
2022-04-26 17:51:41 +0300

    (feat) add manual option to Help JMenu

--- src/main/java/com/graphics/ZooFrame.java
+++ src/main/java/com/graphics/ZooFrame.java
@@ -44,6 +44,7 @@ public class ZooFrame extends JFrame{
 
         // initiating items for help menu.
         JMenuItem helpMenuItem = new JMenuItem("Help");
+        JMenuItem manualMenuItem = new JMenuItem("Manual");
 
 
         file.add(exit);
@@ -53,6 +54,7 @@ public class ZooFrame extends JFrame{
         background.add(none);
 
         help.add(helpMenuItem);
+        help.add(manualMenuItem);
 
         // adding menus to the menu bar.
         menuBar.add(file);
@@ -65,6 +67,7 @@ public class ZooFrame extends JFrame{
         green.addActionListener(listener);
         none.addActionListener(listener);
         helpMenuItem.addActionListener(listener);
+        manualMenuItem.addActionListener(listener);
 
         return menuBar;
     }
@@ -79,19 +82,38 @@ public class ZooFrame extends JFrame{
                 }
                 case "Image" -> {
                     zooPanel.setImageBackground();
-                    System.out.println("Background -> Image pressed.");
                 }
                 case "Green" -> {
                     zooPanel.setGreenBackground();
-                    System.out.println("Background -> Green pressed.");
                 }
                 case "None" -> {
-                    System.out.println("Background -> None pressed!");
                     zooPanel.setNoBackground();
                 }
                 case "Help" -> {
-                    JOptionPane.showMessageDialog(null,"Home Work 2\nGUI", "Message", JOptionPane.INFORMATION_MESSAGE);
-                    System.out.println("Help -> Help pressed.");
+                    String message = "Home Work 2\nGUI";
+                    try {
+                        throw new PrivateGraphicUtils.InformationDialogException(null, message);
+                    } catch (PrivateGraphicUtils.InformationDialogException ignored) {}
+                }
+                case "Manual" -> {
+                    String message = """
+                            OPTIONS:
+                            1.Add animals- lets you add animals to the zoo, up to 10 animals in the zoo
+                            2.Move animals- lets you move the animals to another location
+                            3.Clear - deletes all the animals in the zoo, doesn't affects food though.
+                            4.Add food - lets you choose the type of food: Lettuce, Meat, Cabbage.
+                            5.Info- lets you see the zoo animals information.
+                            6.Exit - exits the program.
+                            
+                            IMPORTANT SIDE NOTES:
+                            *When adding a new animal please check that there are no predators on the same coordinates, otherwise the animal will be instantly eaten
+                            *When adding a new animal red text fields indicate invalid input and black text fields indicate valid input
+                            *Food spawns in the middle of the screen(400, 300).
+                            *A predator can eat more than a single prey at a time
+                            *We recommend to leave the info table open, it will exhibit every information changes about the animals dynamically.""";
+                    try {
+                        throw new PrivateGraphicUtils.InformationDialogException(null, message);
+                    } catch (PrivateGraphicUtils.InformationDialogException ignored) {};
                 }
             }
         }
@@ -105,6 +127,5 @@ public class ZooFrame extends JFrame{
                 new ZooFrame();
             }
         });
-//        frame.setLayout(new BorderLayout(100,100));
     }
 }
