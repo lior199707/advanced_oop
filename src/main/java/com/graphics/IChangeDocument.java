6dfb30a5d76847da7118346fd34700677be98fa2
Lior <Shilon199707@gmail.com>
2022-04-28 23:11:59 +0300

    (doc) added documentation to interface: IDrawable, IChangeDocument

--- src/main/java/com/graphics/IChangeDocument.java
+++ src/main/java/com/graphics/IChangeDocument.java
@@ -3,16 +3,32 @@ package com.graphics;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+/**
+ * interface IChangeDocument extends document listener and set default implementation to avoid code duplication.
+ */
 public interface IChangeDocument extends DocumentListener {
     void changeDocument(DocumentEvent e);
+
+    /**
+     * invokes upon insertion to a document.
+     * @param e - DocumentEvent object.
+     */
     @Override
     default void insertUpdate(DocumentEvent e){
         changeDocument(e);
     }
+    /**
+     * invokes upon deleting from a document.
+     * @param e - DocumentEvent object.
+     */
     @Override
     default void removeUpdate(DocumentEvent e){
         changeDocument(e);
     }
+    /**
+     * invokes upon changing a document.
+     * @param e - DocumentEvent object.
+     */
     @Override
     default void changedUpdate(DocumentEvent e){
         changeDocument(e);
